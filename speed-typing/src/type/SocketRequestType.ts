9fa20c62e6421ff06b934f60d76b325dfc056139
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2023-07-21 15:07:01 +0530

    public lobby added

--- speed-typing/src/type/SocketRequestType.ts
+++ speed-typing/src/type/SocketRequestType.ts
@@ -1,6 +1,7 @@
 export enum SocketRequestType {
-    LOBBY_JOIN = 'joinLobby',
-    LOBBY_TOGGLE_READY = 'toggleReady',
-    LOBBY_SWITCH_CAR = 'switchCar',
-    GAME_UPDATE = 'gameUpdate',
+  LOBBY_JOIN = "joinLobby",
+  LOBBY_JOIN_PUBLIC = "joinLobbyPublic",
+  LOBBY_TOGGLE_READY = "toggleReady",
+  LOBBY_SWITCH_CAR = "switchCar",
+  GAME_UPDATE = "gameUpdate",
 }
